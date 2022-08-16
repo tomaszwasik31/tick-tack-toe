dbdd612f4169c4f03bf418f85eb5045cc8d7c822
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-08-16 15:31:18 +0100

    logic, base structure

---
+++ tick_tack_toe.rb
@@ -0,0 +1,20 @@
+row1 = [1, 2, 3]
+row2 = [4, 5, 6]
+row3 = [7, 8, 9]
+
+def print_grid(row1, row2, row3)
+  puts ''
+  puts row1.join('|')
+  puts row2.join('|')
+  puts row3.join('|')
+end
+
+print_grid(row1, row2, row3)
+
+mark='x'
+def put_mark(mark_postion, mark_type)
+  
+
+  end
+end
+
